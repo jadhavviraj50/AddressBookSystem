aaed8d1a4f1689ab60f758d09db6626307767758
Viraj <jadhavviraj50@gmail.com>
2022-04-28 21:33:01 +0530

    [Viraj]-Added UC1CreateContact file

---
+++ AddressBookSystem/AddressBookSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AddressBook;
+
+namespace AddressBook
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("------WelCome To Address Book Program------");
+            Contacts contact = new Contacts();
+            AddAddressBook book = new AddAddressBook();
+            book.AddContacts();
+            book.Display();
+
+        }
+    }
+}
